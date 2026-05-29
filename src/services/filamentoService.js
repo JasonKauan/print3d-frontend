f5995168e017b85b46a515c41599d0b924dd40bb
JasonKauan <jasonkauandiass@gmail.com>
2026-05-28 22:00:05 -0300

    fix: rename listar function to listarDisponiveis for clarity

--- src/services/filamentoService.js
+++ src/services/filamentoService.js
@@ -2,7 +2,7 @@ import api from './api'
 
 export const filamentoService = {
   listar:           ()       => api.get('/filamentos'),
-  listar:()       => api.get('/filamentos?disponiveis=true'),
+  listarDisponiveis: ()      => api.get('/filamentos?disponiveis=true'),
   buscar:           (id)     => api.get(`/filamentos/${id}`),
   criar:            (data)   => api.post('/filamentos', data),
   atualizar:        (id, data) => api.put(`/filamentos/${id}`, data),
